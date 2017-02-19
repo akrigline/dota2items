26ee4a994f995d94872a09c8200a8bf5081db002
Andrew Krigline <a.krigline@gmail.com>
2017-02-19 14:19:20 -0500

    Updated ItemHelper to use a merged version of two json files for more information.

--- src/utils/itemHelper.js
+++ src/utils/itemHelper.js
@@ -1,3 +1,5 @@
+import {merge} from 'lodash';
+
 const removedItems = [
   'cheese_lg.png?3',
   'aegis_lg.png?3',
@@ -8,16 +10,31 @@ const removedItems = [
   'river_painter5_lg.png?3',
   'river_painter6_lg.png?3',
   'river_painter7_lg.png?3'
-]
+];
+const removedShopItems = [
+  'item_cheese',
+  'item_aegis',
+  'item_river_painter',
+  'item_river_painter2',
+  'item_river_painter3',
+  'item_river_painter4',
+  'item_river_painter5',
+  'item_river_painter6',
+  'item_river_painter7'
+];
 // qual: epic, artifact, component
 export function compileItemArray() {
-  let items = require('../constants/itemData.json');
+  const items = require('../constants/itemData.json');
   let itemData = items.itemdata;
   itemData = Object.keys(itemData).map(key => itemData[key]);
-
   itemData = cleanItemData(itemData);
 
-  return itemData;
+  const itemShops = require('../constants/items.json');
+  let itemShopsData = itemShops.result.items;
+  itemShopsData = cleanShopsData(itemShopsData);
+
+  let mergedArrays = merge(itemData, itemShopsData);
+  return mergedArrays;
 }
 
 function cleanItemData(itemData) {
@@ -27,6 +44,23 @@ function cleanItemData(itemData) {
     },
     removedItems
   );
-  console.log(itemData.length, filtered.length);
+  return filtered;
+}
+
+function cleanShopsData(shopsData) {
+  let filtered = shopsData.filter(
+    function(item) {
+      return this.indexOf(item.name) < 0;
+    },
+    removedShopItems
+  );
+  filtered = filtered.filter(
+    function(item) {
+      if (item.recipe !== 1) {
+        return true;
+      }
+      return false;
+    }
+  );
   return filtered;
 }
